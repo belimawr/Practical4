4e0bb0d24016de6d9bd3d4ec28b00e04aad36ba7
Tiago Queiroz <contato@tiago.eti.br>
2013-02-20 12:05:21 +0000

    Contact page, just the template

---
+++ contact.php
@@ -0,0 +1,39 @@
+<html>
+ <HEAD>
+  <link rel="stylesheet" type="text/css" href="index.css">
+ </HEAD>
+ <body>
+	<?php include 'templates.php'; ?>
+
+		<div id="header">
+			<div id="headerlogo"> <img id="logo" src="logo.jpg" alt="logo"> </div>
+			<div id="title"><h1>ICWD 2014 </br> May 15th - 20th 2014 </br> Glasgow, United Kingdom </h1> </div>
+
+		</div>
+		
+		<div id="container">	
+
+		<div id="menu">
+			<h2 id="navigation"> Navigation </h2>
+			<ul>
+				<li><a href=addEntry2.html>Papers Submission</a></li></br>
+				<li><a href=viewAll.php>Key Dates </a></li></br>
+				<li><a href=viewAll.php>Registration </a></li></br>
+				<li><a href=viewAll.php>Workshops </a></li></br>
+				<li><a href=contact.php>Contact Us </a></li></br>
+				<li><a href=viewAll.php>Sponsors </a></li></br>
+
+			</ul>
+		</div>
+
+		<div id="content">
+			PUT THE CONTENT HEre
+		</div>
+
+		<?php print_footnote(); ?>
+	
+	</div>
+ </body>
+</html>
+
+

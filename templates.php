ca5ea6d999fa574e2c0c346415fe666895b7ce55
larissaleite <larissa.leite@gmail.com>
2013-02-21 19:43:58 +0000

    Update templates.php

--- templates.php
+++ templates.php
@@ -17,14 +17,31 @@ EOT;
 			<!--<h2 id="navigation"> Menu </h2>-->
 			<ul>
 				<li><span><a href=index.html>Home</a></span></li>
-				<li><span><a href=papers.html>Papers Submission</a></span></li>
-				<li><span><a href=keydates.html>Key Dates </a></span></li>
-				<li><span><a href=registration.html>Registration </a></span></li>
-				<li><span><a href=workshops.html>Workshops </a></span></li>
-				<li><span><a href=sponsors.html>Sponsors </a></span></li>
+				<li><span><a href=addEntry2.html>Papers Submission</a></span></li>
+				<li><span><a href=viewAll.php>Key Dates </a></span></li>
+				<li><span><a href=viewAll.php>Registration </a></span></li>
+				<li><span><a href=viewAll.php>Workshops </a></span></li>
+				<li><span><a href=viewAll.php>Sponsors </a></span></li>
 
 			</ul>
+
+			<div id="contact">
+				<h2> Contact Us </h2>
+				<form>
+					Name: <input type="text" name="name"><br>
+					E-mail: <input type="text" name="email"><br><br>
+					<textarea rows="10" cols="20"> Type your message here </textarea><br><br>
+					<input id="sub" type="submit" value="Submit">
+				</form>
+			</div>
+			<br><br><br><br>
+			<a href=http://facebook.com><img id="f_logo" src="f_logo.png" alt="facebook_logo"></a> <span id="f_text"> Find us on Facebook! </span><br><br><br><br><br><br>
+			<!--<a href="https://twitter.com/icwd2014" class="twitter-follow-button" style="margin-left:23px;" data-show-count="false" data-size="large">Follow @icwd2014</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> <!-- THIS ONLY WORKS RUNNING ON A SERVER -->
+			
 		</div>
+
+			<a href=http://facebook.com><img id="f_logo" src="f_logo.png" alt="facebook_logo"></a> <span id="f_text"> Find us on Facebook! </span><br><br><br><br><br><br>
 EOT;
 		print($menu);
 

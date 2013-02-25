420baac23e8bde9cd1d8390d3e181315ee6af84f
Tiago Queiroz <contato@tiago.eti.br>
2013-02-25 09:47:45 +0000

    Fixed a small bug

--- templates.php
+++ templates.php
@@ -40,6 +40,7 @@ EOT;
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> <!-- THIS ONLY WORKS RUNNING ON A SERVER -->
 			
 		</div>
+EOT;
 
 		print($menu);
 

5cbde8e9072aa20bd64f800d8329f26348e7aff9
Tiago de França Queiroz <contato@tiago.eti.br>
2013-02-26 20:48:16 +0000

    Database values

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+	/* DB connection data */
+	$host = "s.tiago.eti.br";
+	$database = "StrathWEB_conference";
+	$user = "StrathWEB";
+	$pass = "2013StrathWEB";
+	$contactTable = "contact";
+?>

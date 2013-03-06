bb7841f0cd92a69cdbef272fb144fbe89c7f3c44
Tiago Queiroz <contato@tiago.eti.br>
2013-03-06 14:32:10 +0000

    sponsors

---
+++ sponsors.php
@@ -0,0 +1,36 @@
+<html> 
+
+<body>
+	<?php include 'templates.php'; ?>
+	 <link rel="stylesheet" type="text/css" href="index.css">
+
+		<?php name_logo(); ?>	
+		<div id="container">
+		
+		
+
+			<?php print_menu(); ?>
+			<div id="content">
+				<h1> Sponsors </h1>
+				<div class ="speaker">
+					<img class ="floatImageLeft" src="strath.png" alt="Strathclyde University"> 		
+				</div>
+				
+				<div class="speaker"> 					
+					<img class ="floatImageRight" src="edinburgh.png" alt="Edinburgh University"> 
+				</div>
+			 
+			    <div class="speaker"> 
+					<img class ="floatImageLeft" src="dundee.png" alt="Dundee University">				
+				</div>			  
+			 
+				<div class="speaker"> 
+					<img class ="floatImageRight" src="callie.png" alt="Caledonia University"> 				
+				</div>
+		</div>
+		<?php print_footnote() ?>
+	</div>
+ </body>
+</html>
+
+
